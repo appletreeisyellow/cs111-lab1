6f2316914ff31691ce22eacbc4610adbaff86f31
appletreeisyellow <yccvivo@gmail.com>
2015-11-03 21:34:12 -0800

    Test files

---
+++ test1.sh
@@ -0,0 +1,19 @@
+cat < miao.txt
+
+
+cat < miao1.txt
+
+
+echo hello > miao2.txt
+
+
+cat < miao3.txt
+
+
+cat miao2.txt miao3.txt
+
+
+sort miao.txt > a.txt
+
+
+echo abc
---
+++ test2.sh
@@ -0,0 +1,16 @@
+echo abcdefg > miao.txt
+
+
+cat < miao3.txt > miao.txt
+
+
+cat miao.txt
+
+
+echo ahahah > miao.txt
+
+
+cat miao3.txt
+
+
+echo mooooooo > miao3.txt 
