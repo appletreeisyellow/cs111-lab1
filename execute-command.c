fb1ad8710b12b49ef9aa6a988c1a40c95d4dee17
kexinyu <kexinyu@ucla.edu>
2015-11-04 18:30:55 -0800

    Update execute-command.c

--- execute-command.c
+++ execute-command.c
@@ -1,5 +1,6 @@
 // UCLA CS 111 Lab 1 command execution
 
+#include <alloc.h>
 #include "command.h"
 #include "command-internals.h"
 
@@ -7,13 +8,15 @@
 #include <sys/wait.h>
 
 #include <error.h>
-#include <sys/wait.h>	// waitpid() 
-#include <unistd.h>		// fork(), pipe(), execvp()
-#include <stdio.h>		// freopen()
-#include <stdlib.h>		// exit()
+#include <sys/wait.h>	// waitpid()
+#include <unistd.h>	// fork(), pipe(), execvp()
+#include <stdio.h>	// freopen()
+#include <stdlib.h>	// exit()
+#include <string.h>     // strcpy(), strcat()
 
+static int DEBUG_EXPAND;
 
-
+char* my_string(char* first, char* second);
 int command_status(command_t c);
 void execute_pipe(command_t c);
 void execute(command_t c);
@@ -21,10 +24,21 @@ void execute_sequence(command_t c);
 int execute_command_type(command_t c);
 void close_and_check(int pipe);
 
+
+char* my_string(char* first, char* second)
+{
+    if (!(first && second)) return NULL;
+    char* r = checked_malloc(sizeof(char) * (strlen(first) + strlen(second) + 1));
+    strcpy(r, first);
+    strcat(r, second);
+    return r;
+}
+
+
 int
 command_status (command_t c)
 {
-  return c->status;
+    return c->status;
 }
 
 
@@ -38,10 +52,10 @@ void execute_pipe(command_t c)
     {
         fprintf(stderr, "ERROR: Creating pipe failed\n");
     }
-   
+    
     if((left=fork()) == -1){
-		fprintf(stderr, "ERROR: Failed to create new process.\n");
-	}
+        fprintf(stderr, "ERROR: Failed to create new process.\n");
+    }
     else if( left == 0 )//left child
     {
         close_and_check(mypipe[0]);
@@ -58,15 +72,15 @@ void execute_pipe(command_t c)
     else//parent
     {
         if((right=fork()) == -1){
-			fprintf(stderr, "ERROR: Failed to create new process.\n");
-		}
+            fprintf(stderr, "ERROR: Failed to create new process.\n");
+        }
         else if(right == 0)//right child
         {
             close_and_check(mypipe[1]);
             if(dup2(mypipe[0],0)==-1)
             {
                 //check for error
-				fprintf(stderr, "ERROR: Duplicating file descriptors failed.\n");
+                fprintf(stderr, "ERROR: Duplicating file descriptors failed.\n");
             }
             close_and_check(mypipe[0]);
             execute_command_type(c->u.command[1]);
@@ -97,30 +111,43 @@ void execute_pipe(command_t c)
 void execute(command_t c)
 {
     pid_t cpid;
-	
-	if((cpid=fork()) == -1)
-	{
-		fprintf(stderr, "ERROR: Filed to create a new process.\n");
-	}
+    
+    if((cpid=fork()) == -1)
+    {
+        fprintf(stderr, "ERROR: Filed to create a new process.\n");
+    }
     else if(cpid==0)//child
     {
         //open up file descriptors
         if(c->input != NULL){
-			if (freopen(c->input, "r", stdin) == NULL){
-				fprintf(stderr, "ERROR: File open failed.\n");
-				exit(1);
-			}
+            if (freopen(c->input, "r", stdin) == NULL){
+                fprintf(stderr, "ERROR: File open failed.\n");
+                exit(1);
+            }
         }
         if(c->output != NULL){
             if(freopen(c->output, "w", stdout) == NULL){
-				fprintf(stderr, "ERROR: File write failed.\n");
-				exit(1);
-			}
+                fprintf(stderr, "ERROR: File write failed.\n");
+                exit(1);
+            }
+        }
+        
+        if (DEBUG_EXPAND)
+        {
+            char* cmd = "";
+            int i = 0;
+            while (c->u.word[i])
+            {
+                cmd = my_string(cmd, " ");
+                cmd = my_string(cmd, c->u.word[i]);
+                i++;
+            }
+            fprintf(stderr, "+%s\n", cmd);
         }
         
         if(execvp(c->u.word[0], c->u.word) < 0){
             fprintf(stderr, "ERROR: Failed to execute the command.\n");
-			exit(1);
+            exit(1);
         }
         fclose(stdin);
         fclose(stdout);
@@ -135,37 +162,37 @@ void execute(command_t c)
 }
 
 void execute_sequence(command_t c) {
-	int child_status;
-	pid_t left, right;
-
-	if ((left = fork()) == -1) {
-		fprintf(stderr, "ERROR: Failed to create new process.\n");
-		exit(1);
-	}
-	else if (left == 0)//left child
-	{
-		execute_command_type(c->u.command[0]);
-		exit(c->u.command[0]->status);
-	}
-	else//parent
-	{
-		waitpid(left, &child_status, 0);
-
-		if ((right = fork()) == -1) {
-			fprintf(stderr, "ERROR: Failed to create new process.\n");
-			exit(1);
-		}
-		else if (right == 0)//right child
-		{
-			execute_command_type(c->u.command[1]);
-			exit(c->u.command[1]->status);
-		}
-		else//parent
-		{
-			waitpid(right, &child_status, 0);
-			c->status = WEXITSTATUS(child_status);
-		}
-	}
+    int child_status;
+    pid_t left, right;
+    
+    if ((left = fork()) == -1) {
+        fprintf(stderr, "ERROR: Failed to create new process.\n");
+        exit(1);
+    }
+    else if (left == 0)//left child
+    {
+        execute_command_type(c->u.command[0]);
+        exit(c->u.command[0]->status);
+    }
+    else//parent
+    {
+        waitpid(left, &child_status, 0);
+        
+        if ((right = fork()) == -1) {
+            fprintf(stderr, "ERROR: Failed to create new process.\n");
+            exit(1);
+        }
+        else if (right == 0)//right child
+        {
+            execute_command_type(c->u.command[1]);
+            exit(c->u.command[1]->status);
+        }
+        else//parent
+        {
+            waitpid(right, &child_status, 0);
+            c->status = WEXITSTATUS(child_status);
+        }
+    }
 }
 
 int execute_command_type(command_t c)
@@ -203,21 +230,22 @@ int execute_command_type(command_t c)
             break;
             
         case SEQUENCE_COMMAND: // return the status of the last command
-			execute_sequence(c);
+            execute_sequence(c);
             break;
     }
     return c->status;
 }
 
 void close_and_check(int pipe){
-	if(close(pipe) == -1){
-		fprintf(stderr, "ERROR: Failed to close a file descriptor.\n");
-		exit(1);
-	}
+    if(close(pipe) == -1){
+        fprintf(stderr, "ERROR: Failed to close a file descriptor.\n");
+        exit(1);
+    }
 }
 
 void
-execute_command (command_t c, int time_travel)
+execute_command (command_t c, int time_travel,int debug_expand)
 {
+    DEBUG_EXPAND=debug_expand;
     execute_command_type(c);
 }
